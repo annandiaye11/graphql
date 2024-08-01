1980ab6201282ec839df8bb74008254f83089aa9
TanakAiko <cherifmbaye02@gmail.com>
2024-08-01 16:58:24 +0000

    graphql

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), import("tailwindcss"), import("autoprefixer")],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
     },
   },
 });
